951376e9300a50bd6d96a038275aa718e808ca5e
teja vvm <tejavvm@macbookair.mynetworksettings.com>
2025-04-21 16:11:28 -0400

    Final version before cloud deploy

--- src/main/java/com/example/emailapp/EmailAppApplication.java
+++ src/main/java/com/example/emailapp/EmailAppApplication.java
@@ -1,6 +1,7 @@
 
 package com.example.emailapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmailAppApplication {
 
     public static void main(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        // Just for debugging (optional)
+        System.out.println("Loaded DB URL: " + dotenv.get("MYSQL_URL"));
+
         SpringApplication.run(EmailAppApplication.class, args);
     }
 }
